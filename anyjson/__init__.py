dc79d65000ba1727134ccde954f23f43806524da
Rune Halvorsen <runefh@gmail.com>
2012-06-21 16:21:41 +0200

    Updated changelog and bumped version

--- anyjson/__init__.py
+++ anyjson/__init__.py
@@ -3,7 +3,7 @@ interface"""
 
 import sys
 
-VERSION = (0, 3, 1)
+VERSION = (0, 3, 2)
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Rune Halvorsen"
 __contact__ = "runefh@gmail.com"
